f314ae58e52bef9881feeb3ae42b1168d8a08dd1
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-04-14 20:08:51 -0300

    Adicionando o menu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int controle = -1; // Variável de controle
+        // Variável do tipo Scanner
+        // utilizada para fazer a leitura dos
+        // dados a partir do teclado.
+        Scanner entrada = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//      Criando um menu para executar
+//      a atividade solicitada
+        do {
+            System.out.println("Lista de exercícios 2 - PSC - FASEH"); // Mosta "texto" na tela
+            System.out.println("Escolha abaixo o exercício que deseja executar."); // Mosta "texto" na tela
+            System.out.println("\t1. Criar menu");
+            System.out.println("\t2. A + B < C?");
+            System.out.println("\t3. Número par ou ímpar?");
+            System.out.print("Informe qual a opção desejada (-1 para sair):\\> ");
+            controle = entrada.nextInt(); // Faz a leitura de um número inteiro
+
+            switch (controle) {
+                case -1:
+                    System.out.println("Tem certeza que deseja sair?");
+                    System.out.println("(S)im; (N)ão" );
+                    String confirmacao;
+                    // Le uma string e pega a primeira posição.
+                    confirmacao = entrada.next();
+
+                    if (confirmacao.equalsIgnoreCase("S")) {
+                        System.out.println("Programa encerrado");
+                        return;
+                    } else {
+                        System.out.println("Retornando...\n");
+                        controle = 99;
+                    }
+                    break;
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    break;
+            }
+
+        } while (controle != -1);
     }
 }
