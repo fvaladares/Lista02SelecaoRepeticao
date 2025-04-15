072eda4310313b901a96188f2382da3dff8c1a66
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-04-14 21:35:15 -0300

    Comentário adicionado.

--- src/ExercicioPreDesafio.java
+++ src/ExercicioPreDesafio.java
@@ -3,6 +3,7 @@ public class ExercicioPreDesafio {
     public void executar() {
         int maxI = 10;
         int maxJ = 10;
+        //todo(Alterar o algoritmo para exibir as coordenadas de cada ponto no formato [i,j] (espaço).
         for (int i = 0; i < maxI; i++) {
             for (int j = 0; j < maxJ; j++) {
                 System.out.print(j + ", ");
